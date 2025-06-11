acbb941b11371f16ac85c20d647b3c9636b85947
Nagin <naginbanodha@gmail.com>
2025-06-12 02:27:36 +0530

    issus fixed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export { default as createMaggie } from "./routes";
+import express from "express";
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+});
