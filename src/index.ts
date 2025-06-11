59c668e058c4c137574ccd11b4f081e2be9f25bb
Nagin <naginbanodha@gmail.com>
2025-06-12 02:28:52 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import express from "express";
-
-const app = express();
-
-app.listen(5000, () => {
-  console.log("listening on port 5000");
-});
+export { default as createMaggie } from "./routes";
